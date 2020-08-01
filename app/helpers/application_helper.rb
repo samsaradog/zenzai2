9aa90242e131c0e65933ae3204f9c8f3129d84fb
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2020-08-01 15:40:21 -0400

    Bootstrap working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,55 +1,63 @@
 module ApplicationHelper
   def header_tabs
-    sanitize("#{tabs_header}#{user_profile}#{sign_in_sign_up}#{admin_tabs}#{sign_out}#{tabs_footer}", attributes: %w[data-method href class])
+    raw("#{tabs_header}#{user_profile}#{sign_in_sign_up}#{admin_tabs}#{sign_out}#{tabs_footer}")
+    #sanitize("#{tabs_header}#{user_profile}#{sign_in_sign_up}#{admin_tabs}#{sign_out}#{tabs_footer}", attributes: %w[data-method href class])
   end
 
   def tabs_header
     <<-html
-    <div class="fixed">
-      <nav class="top-bar" data-topbar="">
-        <section class="top-bar-section">
-          <ul class="left">
-            <li class="active"><a href="/" class="tab">Home</a></li>
-            <li><a href="/pages/about" class="tab">About</a></li>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="/pages/about" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 About
+               </a>
+               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                 <a class="dropdown-item" href="/pages/about">Welcome</a>
+                 <a class="dropdown-item" href="/pages/zenzai">Zenzai</a>
+                 <a class="dropdown-item" href="/pages/support">Support</a>
+               </div>
+            </li>
     html
   end
 
   def user_profile
     return unless current_user
     <<-html
-            <li><a href="/users/edit" class="tab">Profile</a></li>
+            <li><a href="/users/edit" class="nav-link">Profile</a></li>
     html
   end
 
   def sign_in_sign_up
     return if current_user
     <<-html
-            <li><a href="/users/sign_in" class="tab">Sign in</a></li>
-            <li><a href="/users/sign_up" class="tab">Sign up</a></li>
+            <li><a href="/users/sign_in" class="nav-link">Sign in</a></li>
+            <li><a href="/users/sign_up" class="nav-link">Sign up</a></li>
     html
   end
 
   def sign_out
     return unless current_user
     <<-html
-            <li>#{link_to('Sign out', '/users/sign_out', method: 'delete', class: 'tab')}</li>
+            <li>#{link_to('Sign out', '/users/sign_out', method: 'delete', class: 'nav-link')}</li>
     html
   end
 
   def admin_tabs
     return unless current_user&.is_admin?
     <<-html
-            <li><a href="/users" class="tab">Users</a></li>
-            <li><a href="/jewels" class="tab">Jewels</a></li>
+            <li><a href="/users" class="nav-link">Users</a></li>
+            <li><a href="/jewels" class="nav-link">Jewels</a></li>
     html
   end
 
   def tabs_footer
     <<-html
           </ul>
-        </section>
+        </div>
       </nav>
-    </div>
     html
   end
 end
