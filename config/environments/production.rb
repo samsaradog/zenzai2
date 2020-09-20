8677e0f14ec9c3e9b45d1d4c757a5e1694346c00
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2020-09-20 16:10:26 -0400

    Adds production configuration for Amazon SES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'zenzaizenzai.com' }
 
+  # Configure to use Amazon SES
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "email-smtp.us-east-2.amazonaws.com",
+      :port => 587, # Port 25 is throttled on AWS
+      :user_name => ENV['SMTP_USERNAME'],
+      :password => ENV['SMTP_PASSWORD'],
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
